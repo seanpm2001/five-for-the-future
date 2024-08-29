f2e80b8d070bef4322fd0ffae8c14ea6cd80695f
Kelly Dwan <ryelle@users.noreply.github.com>
2024-08-29 16:38:40 -0400

    Blocks: Add missing component import

--- themes/wporg-5ftf-2024/src/pledge-contributors/index.js
+++ themes/wporg-5ftf-2024/src/pledge-contributors/index.js
@@ -3,6 +3,7 @@
  */
 import { registerBlockType } from '@wordpress/blocks';
 import { useBlockProps } from '@wordpress/block-editor';
+import ServerSideRender from '@wordpress/server-side-render';
 
 /**
  * Internal dependencies
