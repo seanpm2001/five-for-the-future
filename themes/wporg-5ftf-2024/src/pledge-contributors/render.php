cf4c3db626376952b29d0a25b4077ee8c430192e
Paul Kevan <2290623+pkevan@users.noreply.github.com>
2024-09-13 11:51:59 +0100

    Change rendered avatar size
    Reduces the standard fetched avatar size, dependant on the format.

--- themes/wporg-5ftf-2024/src/pledge-contributors/render.php
+++ themes/wporg-5ftf-2024/src/pledge-contributors/render.php
@@ -16,9 +16,13 @@ $is_truncated = isset( $attributes['className'] ) && str_contains( $attributes['
 // Initialize count to zero for untruncated view.
 $count_more = 0;
 
+// Set avatar size at smallest usage in px.
+$avatar_size = 30;
+
 if ( $is_truncated ) {
 	$count_more   = count( $contributors ) - TRUNCATED_MAX;
 	$contributors = array_splice( $contributors, 0, TRUNCATED_MAX );
+	$avatar_size  = 70;
 }
 
 ?>
@@ -31,7 +35,7 @@ if ( $is_truncated ) {
 				<li class="pledge-contributor">
 					<span class="pledge-contributor__avatar">
 						<a href="<?php echo esc_url( 'https://profiles.wordpress.org/' . $contributor->user_nicename . '/' ); ?>">
-							<?php echo get_avatar( $contributor->user_email, 280, 'mystery', $contributor->display_name ); ?>
+							<?php echo get_avatar( $contributor->user_email, $avatar_size, 'mystery', $contributor->display_name ); ?>
 						</a>
 					</span>
 				</li>
